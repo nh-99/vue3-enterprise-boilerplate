3273d060755a0a1762ce85bfb294162b8842e12b
Noah Howard <3317164+nh-99@users.noreply.github.com>
2024-01-30 20:57:30 -0500

    idek

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,9 +1,8 @@
-import { RequestHandler } from 'msw'
 import getUsers from './resources/users'
 import getMessage from './resources/message'
 // pinia-mock-import - DO NOT REMOVE
 
-export const handlers: RequestHandler[] = [
+export const handlers: any = [
   getUsers(),
   getMessage()
   // pinia-mock-handler - DO NOT REMOVE
